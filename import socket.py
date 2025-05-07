f1e79acff9518420718d6158889b129798883977
Esunxuan <630049245@qq.com>
2025-05-07 21:45:22 +0800

    commit

--- import socket.py
+++ import socket.py
@@ -2,7 +2,6 @@ import socket
 import threading
 import time
 
-# 全局变量
 tuple_space = {}
 operation_count = 0
 put_count = 0
@@ -10,3 +9,6 @@ read_count = 0
 get_count = 0
 error_count = 0
 client_count = 0
+
+def handle_client(client_socket):
+    global operation_count, put_count, read_count, get_count, error_count
